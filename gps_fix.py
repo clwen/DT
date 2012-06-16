7cf2d38f4d13463cf40142e3d39928cfd4988ca3
Chung-Lin Wen <jonathan.clwen@gmail.com>
2012-06-16 18:25:26 -0400

    skeleton for venue attendance, in the middle of parsing file

--- gps_fix.py
+++ gps_fix.py
@@ -1,10 +1,10 @@
 class GPSFix:
 
-    def __init__(self, time, lon, lat):
+    def __init__(self, time, lon, lat, venues_inside = []):
         self.time = time
         self.lon = lon
         self.lat = lat
-        venues_inside = []
+        self.venues_inside = venues_inside
 
     def update_venues_inside(self, venue_lst):
         self.venues_inside = venue_lst
--- venue_attendance.py
+++ venue_attendance.py
@@ -5,6 +5,27 @@ class DeviceDatePair:
     def __init__(self):
         self.gps_fixes = []
 
+def read_device_date_pair_from_file(date, device, distance):
+    filename = 'GPSInsideVenue/%s/%s/%s.txt' % (date, distance, device)
+    with open(filename) as f:
+        lines = f.readlines()
+        lines = lines[1:] # discard first line, which is comment
+        tokens = lines[711].split(' ')
+        # print tokens[0]
+        # print tokens[1]
+        # print tokens[2]
+        print tokens[3]
+        # for token in tokens:
+        #     print token
+
+def parse_venue_attendance(date, device, distance):
+    pass
+
 if __name__ == '__main__':
-    print 'hello'
+    # read device date pair from file
+    read_device_date_pair_from_file('20110421', '17', '15M')
+
+    # parse venue attendance from 
+    # parse_venue_attendance('20110421', '17', '15M')
+
 
