4b32d0a6cccdbcaa6b251bcb1c006fc7a2737dd3
Chung-Lin Wen <jonathan.clwen@gmail.com>
2012-06-17 14:38:34 -0400

    load gps fixes from json file; parse and output to class member function

--- venue_attendance.py
+++ venue_attendance.py
@@ -1,31 +1,45 @@
 import os
+import simplejson as json
 from gps_fix import *
 
+# dates = ['20110420', '20110421', '20110422', '20110423', '20110424', '20110425', '20110426', '20110427', '20110428', '20110429', '20110430']
+dates = ['20110420']
+# dates = ['20110421', '20110422', '20110423', '20110424', '20110425', '20110426', '20110427', '20110428', '20110429', '20110430']
+distances = ['0M', '5M', '10M', '15M']
+
 class DeviceDatePair:
-    def __init__(self):
-        self.gps_fixes = []
+
+    def __init__(self, date, device, distance, gps_fixes):
+        self.gps_fixes = gps_fixes
+        self.date = date
+        self.device = device
+        self.distance = distance
+        self.venues_attended = []
+
+    def parse_venue_attendance(self):
+        print 'parse venue attendance'
+
+    def output_venue_attendance(self):
+        print 'output venue attendance'
 
 def read_device_date_pair_from_file(date, device, distance):
-    filename = 'GPSInsideVenue/%s/%s/%s.txt' % (date, distance, device)
+    filename = 'GPSInsideVenue/%s/%s/%s.json' % (date, distance, device)
+    gps_fixes = []
     with open(filename) as f:
         lines = f.readlines()
-        lines = lines[1:] # discard first line, which is comment
-        tokens = lines[711].split(' ')
-        # print tokens[0]
-        # print tokens[1]
-        # print tokens[2]
-        print tokens[3]
-        # for token in tokens:
-        #     print token
-
-def parse_venue_attendance(date, device, distance):
-    pass
+        for line in lines:
+            gps_fields = json.loads(line)
+            # print gps_fields
+            gps_fix = GPSFix(gps_fields['time'], gps_fields['lon'], gps_fields['lat'], gps_fields['venues_inside'])
+            gps_fixes.append(gps_fix)
+    device_date_pair = DeviceDatePair(date, device, distance, gps_fixes)
+    return device_date_pair
 
 if __name__ == '__main__':
     # read device date pair from file
-    read_device_date_pair_from_file('20110421', '17', '15M')
-
-    # parse venue attendance from 
-    # parse_venue_attendance('20110421', '17', '15M')
-
+    device_date_pair = read_device_date_pair_from_file('20110421', '17', '15M')
+    # parse venue attendance
+    device_date_pair.parse_venue_attendance()
+    # output to file
+    device_date_pair.output_venue_attendance()
 
