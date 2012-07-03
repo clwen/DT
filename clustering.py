fc243c5d0a13a6b863124b2b4e7d7511a4924121
Chung-Lin Wen <jonathan.clwen@gmail.com>
2012-07-03 15:00:40 -0400

    1st version for kmeans

--- clustering.py
+++ clustering.py
@@ -1,4 +1,8 @@
 import csv
+from numpy import *
+from sklearn.cluster import KMeans
+
+n_clusters = 5
 
 # load demo data
 demo_reader = csv.reader(open('demographic/2011_demo.csv'))
@@ -17,3 +21,16 @@ for row in att_reader:
 assert(len(demos) == len(attendances))
 demo_att = [demos[i] + attendances[i] for i in range(len(demos))]
 
+da_array = array(demo_att)
+print da_array
+print type(da_array)
+print da_array.shape
+
+k_means = KMeans(k=n_clusters)
+k_means.fit(da_array)
+k_means_labels = k_means.labels_
+k_means_cluster_centers = k_means.cluster_centers_
+print k_means_labels
+for l in k_means_labels:
+    print l,
+print k_means_cluster_centers
