133d41066a26b46265b0b5fc7efa4a4744f33f72
Chung-Lin Wen <jonathan.clwen@gmail.com>
2012-07-11 15:45:06 -0400

    output clustered result to csv

--- clustering.py
+++ clustering.py
@@ -29,19 +29,32 @@ print type(da_array)
 print da_array.shape
 
 # conduct kmeans clustering
-# k_means = KMeans(k=n_clusters)
-# k_means.fit(da_array)
-# k_means_labels = k_means.labels_
-# k_means_cluster_centers = k_means.cluster_centers_
+k_means = KMeans(k=n_clusters)
+k_means.fit(da_array)
+k_means_labels = k_means.labels_
+k_means_cluster_centers = k_means.cluster_centers_
+# create a list of list, each list contains items match to label l
+groups = [[] for i in range(n_clusters)]
+# append da_array to groups according to label
+for i in range(len(k_means_labels)):
+    label = k_means_labels[i]
+    groups[label].append(da_array[i])
+# output the groups to file
+for i in range(n_clusters):
+    output_file = 'clustering/%s.txt' % i
+    numpy.savetxt(output_file, groups[i], fmt='%1d', delimiter=',')
+    # with open(output_file, 'w') as of:
+    #     for item in groups[i]:
+    #         of.write(str(item) + '\n')
 # print k_means_labels
 # for l in k_means_labels:
 #     print l,
 # print k_means_cluster_centers
 
 # conduct dbscan clustering
-D = distance.squareform(distance.pdist(da_array)) # distance
-S = 1 - (D / numpy.max(D)) # similarity
-db = DBSCAN().fit(S, eps=0.95, min_samples=10)
-labels = db.labels_
-for label in labels:
-    print label,
+# D = distance.squareform(distance.pdist(da_array)) # distance
+# S = 1 - (D / numpy.max(D)) # similarity
+# db = DBSCAN().fit(S, eps=0.95, min_samples=10)
+# labels = db.labels_
+# for label in labels:
+#     print label,
