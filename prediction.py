41bad0a7d9580d308622abeb1f4e534e5b881283
Chung-Lin Wen <jonathan.clwen@gmail.com>
2012-08-02 18:11:08 -0400

    used normalized demog data, avg accu added

--- prediction.py
+++ prediction.py
@@ -4,14 +4,14 @@ from sklearn import svm
 from sklearn import datasets
 
 # import demo data from file
-demo_reader = csv.reader(open('demographic/2011_demo.csv'))
+demo_reader = csv.reader(open('demographic/2011_demo_normalized.csv'))
 lines = list(demo_reader)
 demos = []
 for line in lines:
-    cur_demo = [int(n) for n in line]
+    cur_demo = [float(n) for n in line]
     demos.append(cur_demo)
 
-# improt venue data from file
+# import venue data from file
 att_reader = csv.reader(open('demographic/2011_att.csv'))
 lines = list(att_reader)
 att_dim = len(lines[0])
@@ -22,14 +22,18 @@ for line in lines:
         atts[i].append(int(line[i]))
 
 # for each venue, test the prediction accuracy
+accs = []
 for i in range(att_dim):
     print 'predicting venue %s' % i
     venue = atts[i]
     classifier = svm.SVC(gamma=0.001, C=100.0)
-    classifier = classifier.fit(demos[:-100], venue[:-100])
-    prediction = classifier.predict(demos[-100:])
-    matched = 0
-    for i in range(100):
+    classifier = classifier.fit(demos[:-130], venue[:-130])
+    prediction = classifier.predict(demos[-130:])
+    matched = 0.0
+    for i in range(130):
         if prediction[i] == venue[i]:
             matched += 1
-    print ' matched: %s' % matched
+    acc = matched / 130.0
+    print ' accuracy: %s' % acc
+    accs.append(acc)
+print 'average accuracy: %s' % (sum(accs) / len(accs))
